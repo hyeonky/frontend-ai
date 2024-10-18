49ca61fe7030c857104a2b9cdd91c8e4d94d0f67
hyeonky <hyeon2762@naver.com>
2024-10-18 15:35:51 +0900

    animate

---
+++ 04-js/js/jquery.js
@@ -0,0 +1,32 @@
+// 실행할 코드
+// btn을 클릭했을 때
+// chaining으로 묶을 수 있음
+$(".btn1")
+  .on("mouseenter", function () {
+    // .box를 없애기
+    $(".box").slideDown(500);
+  })
+  .on("mouseleave", function () {
+    // .box를 없애기
+    $(".box").slideUp(500);
+  });
+
+  // 다음 코드
+$(".btn2")
+  .mouseenter(function () {
+    $(".box2").hide();
+  })
+  .mouseleave(function () {
+    $(".box2").show();
+  });
+
+  // animate
+  // .box3가 1초 동안 왼쪽으로 100px 이동
+  $(".btn3")
+  // .btn3을 클릭했을 때 
+  .click(function () {
+    // {실행 코드}, 스피드
+    $(".box3").animate({
+      left: "100px",
+    },1000);
+  });
