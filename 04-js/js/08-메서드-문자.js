62b8618b2ef3b19aee9d66e96fbafcc4d772bea3
hyeonky <hyeon2762@naver.com>
2024-11-19 15:51:41 +0900

    비동기

--- 04-js/js/08-메서드-문자.js
+++ 04-js/js/08-메서드-문자.js
@@ -95,3 +95,54 @@ const list = ['list1', 'list2', 'list3'];
 list.forEach((item) => {
   console.log(item);
 }); // 함수가 3번 반복 실행됨.
+
+const animals = ['Cat', 'Dog', 'Fish'];
+// console.log(array.includes(element, start)); - includes구문
+console.log(animals.includes('cat')); //false
+console.log(animals.includes('토끼')); //false
+
+// 소문자로 변환 후 비교
+// .toLowerCase()
+
+// array.join(separator) - join 구문
+console.log(animals.join(' 그리고 '));
+
+const users2 = [
+  { name: '홍길동', age: 36 },
+  { name: '고길동', age: 12 },
+  { name: '둘리', age: 6 },
+];
+
+// array.map(function(currentValue, index, arr), thisValue) - map구문
+users2.map(function (item) {
+  console.log(item);
+});
+
+const newUsers = users2.map((item) => ({ ...item, email: null }));
+console.log(newUsers);
+
+console.log(arr.pop());
+
+// 배열의 길이를 반환
+console.log(arr.push(4, 5, 6));
+console.log(arr);
+
+const users4 = [
+  { name: '홍길동', age: 36 },
+  { name: '고길동', age: 12 },
+  { name: '둘리', age: 6 },
+];
+
+// 총 나이 합계(reduce)
+const totalAge = users4.reduce(function (total, currentValue) {
+  return total + currentValue.age;
+}, 0);
+console.log(totalAge);
+
+// 평균 나이 구하기
+// const avgAge = 총나이합계 / 유저수
+const avgAge = totalAge / users4.length;
+console.log(avgAge);
+
+const arr2 = [1, 2, 3, 4, 5];
+console.log(avgAge);
